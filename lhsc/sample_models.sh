9a8f77c906e76e350a2f8a1eb8eaec15cb976059
mohitkr <mohitkrg@gmail.com>
2019-08-21 14:42:10 +0200

    updated sh file

--- lhsc/sample_models.sh
+++ lhsc/sample_models.sh
@@ -7,15 +7,15 @@ for n in 5 10 15; do
         for ph in 0 0.1 0.25; do
             for ps in 0.1 0.25; do
                 for pc in 0 0.1; do
-                output="random_n=${n}_k=${k}_ph=${ph}_ps=${ps}"
-                python sample_models.py \
-                    -o $output \
-                    --num-models $NUM_MODELS \
-                    -n $n -k $k \
-                    --perc-hard $ph \
-                    --perc-soft $ps \
-                    --perc-context $pc \
-                    -s 0
+                    output="random_n=${n}_k=${k}_ph=${ph}_ps=${ps}_ps=${pc}"
+                    python sample_models.py \
+                        -o $output \
+                        --num-models $NUM_MODELS \
+                        -n $n -k $k \
+                        --perc-hard $ph \
+                        --perc-soft $ps \
+                        --perc-context $pc \
+                        -s 0
             done
         done
     done
