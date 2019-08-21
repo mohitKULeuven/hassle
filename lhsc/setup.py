5c89463a986d03a3ad773f9f82c0f84b275e8376
Samuel Kolb <samuel.kolb@me.com>
2019-08-21 15:05:29 +0200

    Implemented learner

--- lhsc/setup.py
+++ lhsc/setup.py
@@ -22,7 +22,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'pywmi',  # 'requests', 'maya', 'records',
+    'pywmi', 'numpy'  # 'requests', 'maya', 'records',
 ]
 
 # What packages are optional?
