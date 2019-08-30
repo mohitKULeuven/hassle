6e8150c8a2c15a408a9fc317a41b26571362ea5c
Samuel Kolb <samuel.kolb@me.com>
2019-08-30 12:48:05 +0200

    Formatted

--- lhsc/experiment.py
+++ lhsc/experiment.py
@@ -17,6 +17,7 @@ from .learn import learn_weighted_max_sat
 
 logger = logging.getLogger(__name__)
 
+
 def represent_int(s):
     try:
         int(s)
@@ -24,23 +25,25 @@ def represent_int(s):
     except ValueError:
         return False
 
+
 def cnf_to_model(cnf_file) -> Optional[MaxSatModel]:
-    model=[]
+    model = []
     with open(cnf_file) as fp:
         line = fp.readline()
-        p_ind=0
+        p_ind = 0
         while line:
-            line_as_list=line.strip().split()
-            if len(line_as_list)>0 and p_ind==1 and represent_int(line_as_list[0]):
-                model.append((None,set(map(int,line_as_list))))
-            if p_ind==0 and line_as_list[0]=='p':
-                p_ind=1
+            line_as_list = line.strip().split()
+            if len(line_as_list) > 0 and p_ind == 1 and represent_int(line_as_list[0]):
+                model.append((None, set(map(int, line_as_list))))
+            if p_ind == 0 and line_as_list[0] == "p":
+                p_ind = 1
             line = fp.readline()
     if model:
         return model
     return None
 
-#def model_to_cnf(model,param,path):
+
+# def model_to_cnf(model,param,path):
 #    cnf_file = open(path+'param'+'.cnf', 'w')
 #    filewriter = csv.writer(csvfile, delimiter=' ')
 #    filewriter.writerow(['model_seed','num_hard', 'num_soft', 'n',
@@ -158,34 +161,35 @@ def random_contexts(
     return [set(c) for c in result]
 
 
-def learn_from_random_model(n,clause_length,num_hard,num_soft,
-                            contexts_count,num_literal,model_seed):
+def learn_from_random_model(
+    n, clause_length, num_hard, num_soft, contexts_count, num_literal, model_seed
+):
     logging.basicConfig(level=logging.WARNING)
-    
-#    model_seeds = [444]
+
+    #    model_seeds = [444]
     context_seed_start = [101010]
-    
+
     rng = np.random.RandomState(model_seed)
     m = num_hard + num_soft
-    
-    param=f"_model_seed_{model_seed}_num_hard_{num_hard}_num_soft_{num_soft}_n_{n}__clause_length_{clause_length}_context_count_{context_count}_num_literal_{num_literal}"
-    row=[model_seed,num_hard,num_soft,n,clause_length,context_count,num_literal]
-    pickle_var={}
-#   pickle_vars=pickle.load( open( 'pickles/'+param+'.pickle', "rb" ) )
-#   print(pickle_vars['data'].shape[0])
-#   continue
-    
+
+    param = f"_model_seed_{model_seed}_num_hard_{num_hard}_num_soft_{num_soft}_n_{n}__clause_length_{clause_length}_context_count_{context_count}_num_literal_{num_literal}"
+    row = [model_seed, num_hard, num_soft, n, clause_length, context_count, num_literal]
+    pickle_var = {}
+    #   pickle_vars=pickle.load( open( 'pickles/'+param+'.pickle', "rb" ) )
+    #   print(pickle_vars['data'].shape[0])
+    #   continue
+
     true_model = generate_model(n, clause_length, num_hard, num_soft, rng)
     true_model = [(w / 100 if w else None, clause) for w, clause in true_model]
     print(f"True Model\n{model_to_string(true_model)}")
-    pickle_var["true_model"]=true_model
+    pickle_var["true_model"] = true_model
     rng = np.random.RandomState(context_seed_start)
     contexts = random_contexts(n, contexts_count, 1, num_literal, rng)
 
     for context in contexts:
-#        print(f"Context {context_to_string(context)}")
+        #        print(f"Context {context_to_string(context)}")
         solution = solve_weighted_max_sat(n, true_model, context)
-#        print(f"Solution {solution}")
+        #        print(f"Solution {solution}")
 
         if solution is None:
             print(f"INFEASIBLE CONTEXT {context_to_string(context)}")
@@ -193,9 +197,9 @@ def learn_from_random_model(n,clause_length,num_hard,num_soft,
     data, labels, learning_contexts = get_data_set(
         n, true_model, contexts, rng, allow_negative=True
     )
-    pickle_var["data"]=data
-    pickle_var["labels"]=labels
-    pickle_var["learning_contexts"]=learning_contexts
+    pickle_var["data"] = data
+    pickle_var["labels"] = labels
+    pickle_var["learning_contexts"] = learning_contexts
 
     if logger.isEnabledFor(logging.INFO):
         overview = [
@@ -207,16 +211,15 @@ def learn_from_random_model(n,clause_length,num_hard,num_soft,
             for k in range(len(labels))
         ]
         print(tabulate(overview))
-#        print(data, labels, learning_contexts)
-    
-    start=time.time()
+    #        print(data, labels, learning_contexts)
+
+    start = time.time()
     learned_model = learn_weighted_max_sat(m, data, labels, learning_contexts)
-    end=time.time()
+    end = time.time()
     print(f"Learned model\n{model_to_string(learned_model)}")
-    pickle_var["learned_model"]=learned_model
-    
-    
-    training_error=0
+    pickle_var["learned_model"] = learned_model
+
+    training_error = 0
     if learned_model:
         for k in range(data.shape[0]):
             instance = data[k, :]
@@ -225,22 +228,22 @@ def learn_from_random_model(n,clause_length,num_hard,num_soft,
                 learned_model, instance, learning_contexts[k]
             )
             if label != learned_label:
-                training_error+=1
-#                if label:
-#                    
-#                    incorrect_best_solution=solve_weighted_max_sat(n, learned_model, learning_contexts[k]).astype(np.int)
-#                    logger.warning(
-#                        f"{instance} labeled {learned_label} instead of {label} because of {incorrect_best_solution}"
-#                    )
+                training_error += 1
+                #                if label:
+                #
+                #                    incorrect_best_solution=solve_weighted_max_sat(n, learned_model, learning_contexts[k]).astype(np.int)
+                #                    logger.warning(
+                #                        f"{instance} labeled {learned_label} instead of {label} because of {incorrect_best_solution}"
+                #                    )
                 logger.warning(
                     f"{instance} labeled {learned_label} instead of {label} (learned / true)"
                 )
-#        regret=eval_regret(n, true_model, learned_model)
-#        print("REGRET ", regret)
-        pickle.dump( pickle_var, open( 'pickles/'+param+'.pickle', "wb" ) )
-        csvfile = open('results/results_big'+'.csv', 'a')
-        filewriter = csv.writer(csvfile, delimiter=',')
-        row.extend([end-start,data.shape[0],training_error])
+        #        regret=eval_regret(n, true_model, learned_model)
+        #        print("REGRET ", regret)
+        pickle.dump(pickle_var, open("pickles/" + param + ".pickle", "wb"))
+        csvfile = open("results/results_big" + ".csv", "a")
+        filewriter = csv.writer(csvfile, delimiter=",")
+        row.extend([end - start, data.shape[0], training_error])
         filewriter.writerow(row)
         csvfile.close()
 
@@ -281,34 +284,48 @@ def learn_hard_constraints():
 
 if __name__ == "__main__":
     # logging.basicConfig(level=logging.INFO)
-#    cnf_dir="cnfs/"
-#    for cnf_file in os.listdir(cnf_dir):
-#        print(cnf_to_model(cnf_dir+cnf_file))
-#    exit()
+    #    cnf_dir="cnfs/"
+    #    for cnf_file in os.listdir(cnf_dir):
+    #        print(cnf_to_model(cnf_dir+cnf_file))
+    #    exit()
     model_seeds = [111, 222, 333, 444, 555]
-    num_hard_lst=[5,10,15,20]
-    num_soft_lst=[5,10,15,20]
-    num_vars=[5,10,15]
-    context_counts=[5,10,15,20]
-    csvfile = open('results/results_big'+'.csv', 'w')
-    filewriter = csv.writer(csvfile, delimiter=',')
-    filewriter.writerow(['model_seed','num_hard', 'num_soft', 'n',
-                         'clause_length','context_count','num_literal',
-                         'time','num_example','training_error'])
+    num_hard_lst = [5, 10, 15, 20]
+    num_soft_lst = [5, 10, 15, 20]
+    num_vars = [5, 10, 15]
+    context_counts = [5, 10, 15, 20]
+    csvfile = open("results/results_big" + ".csv", "w")
+    filewriter = csv.writer(csvfile, delimiter=",")
+    filewriter.writerow(
+        [
+            "model_seed",
+            "num_hard",
+            "num_soft",
+            "n",
+            "clause_length",
+            "context_count",
+            "num_literal",
+            "time",
+            "num_example",
+            "training_error",
+        ]
+    )
     csvfile.close()
     for n in num_vars:
-        for clause_length in range(3,max(3, int(2 * n / 5))):
+        for clause_length in range(3, max(3, int(2 * n / 5))):
             for num_hard in num_hard_lst:
                 for num_soft in num_soft_lst:
                     for context_count in context_counts:
-                        for num_literal in range(1,max(2, int(2 * n / 5))):
+                        for num_literal in range(1, max(2, int(2 * n / 5))):
                             for model_seed in model_seeds:
                                 try:
-                                    learn_from_random_model(n,clause_length,
-                                                            num_hard,num_soft,
-                                                            context_count,
-                                                            num_literal,
-                                                            model_seed)
+                                    learn_from_random_model(
+                                        n,
+                                        clause_length,
+                                        num_hard,
+                                        num_soft,
+                                        context_count,
+                                        num_literal,
+                                        model_seed,
+                                    )
                                 except AssertionError as error:
                                     continue
-    # learn_hard_constraints()
